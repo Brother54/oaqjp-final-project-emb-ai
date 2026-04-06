cd817b2fddff6304202281581b2ad6aab4254395
Brother54 <holabrother54@hotmail.com>
2026-04-06 19:21:01 -0400

    Hasta manejo de errores

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,7 +5,11 @@ def emotion_detector(text_a_analizar):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_a_analizar } }
+       
     response = requests.post(url, json = myobj, headers=header)
+    if response.status_code == 400:
+        return {'sadness': None, 'joy': None, 'fear': None, 'disgust': None, 'anger': None, 'dominant_emotion': None}
+ 
     formatted_response = json.loads(response.text)
     resultado = formatted_response['emotionPredictions'][0]['emotion'] 
     dominante = max(resultado, key=resultado.get)
